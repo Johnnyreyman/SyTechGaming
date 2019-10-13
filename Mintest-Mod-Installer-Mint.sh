db8fb23c75309a0bc0327c98b558351ddae7164e
Johnathan Reynolds <31798114+Johnnyreyman@users.noreply.github.com>
2019-10-13 00:19:16 -0500

    Create Mintest-Mod-Installer-Mint.sh

---
+++ Mintest-Mod-Installer-Mint.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+#A simple installer for Minetest on Linux Mint.
+cd ~/Downloads/
+mkdir ./'Minetest Mods'
+cd ./'Minetest Mods'
+Download=$(zenity --entry --text "URL to Download Mod" --title "Download Link?" --entry-text="");
+wget echo $Download
+Rename=$(zenity --entry --text "What is the name of this mod?" --title "Mod Name" --entry-text="");
+mv master.zip $Rename-master.zip
+gksudo "unzip /home/johnathanr/Downloads/'Minetest Mods'/$Rename-master.zip -d /usr/share/games/minetest/mods/"
+zenity --info --text "Your $Rename Mod Installed correctly. Enable it in Minetest to continue"
+minetest
+zenity --info --text "Thank you for using the Minetest installer by SyTechGames!"
